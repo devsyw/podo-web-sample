6fc3d7c11a0e6f976988710c87ae32c1c774dbf8
lde <ledaeun94@gmail.com>
2022-01-02 17:21:00 +0900

    ddddd

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import Samples from "./screen/samples";
 import Ourstack from "./screen/ourstack";
 import Contact from "./screen/contact";
 
+//서버 실행 npm start
+//서버 종료 ctrl + c
+
 /** 다은이 전용, 퍼블용 */
 const App = () => {
 
